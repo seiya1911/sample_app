e5c7172b637d8148c15814c45d561b576de38101
seiya1911 <siy1911cb@gmail.com>
2021-04-30 14:13:26 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '24f44ff565674a0e8737121f93bdb0690c682c10faa119bde6f2a64a2cb5fe24ca0a02b77886aef7202f5051de61d1e425c3f677fa7cf0d9b194771138498f69'
